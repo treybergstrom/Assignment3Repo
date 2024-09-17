db8829b22871a0f633c749c1423c25cccfec16e3
Trey Bergstrom <trey.bergstrom@okstate.edu>
2024-09-17 17:49:25 -0500

    Add project files.

---
+++ Pages/BasicPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TB_Assignment3_GitHub.Pages
+{
+    public class BasicPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
